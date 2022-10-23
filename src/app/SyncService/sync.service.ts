4d65734e1b1fd4c2e38b37cc56645c0815245c44
lucian_blignaut <lucian.blignaut@aspiresoftware.com>
2022-10-23 11:53:48 +0200

    added doc comments and improvements

--- src/app/SyncService/sync.service.ts
+++ src/app/SyncService/sync.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { OnlineStatusType } from 'ngx-online-status';
-import { Observable, timeout, retry, catchError, TimeoutError, throwError, EMPTY, map, share, concat } from 'rxjs';
+import { Observable, timeout, retry, catchError, TimeoutError, EMPTY, map, share, concat, of, throwError } from 'rxjs';
 import { SyncToDo } from 'src/assets/models/syncToDo.model';
 import { ConnectionService } from '../ConnectionService/connection.service';
 import * as localforage from 'localforage';
@@ -12,67 +11,109 @@ import * as _ from 'lodash'
 })
 export class SyncService {
 
-  public HTTP_TIMEOUT: number = 2000
+  public HTTP_TIMEOUT: number = 5000
 
   constructor(private http: HttpClient, private connection: ConnectionService) { }
 
-  public tryPost<T>(url: string, payload: T, params?: HttpParams): Observable<T> {
+  /**
+   * Acts as a proxy for all outgoing HTTP POST requests. If a request could be made successfully,
+   * the response is returned from the request. If a backend error occurs, error is handled accordingly,
+   * if a network error occurs, queue the request for syncing. 
+   * @param url 
+   * @param payload 
+   * @param params 
+   * @returns Observable 
+   */
+  public tryPost<T>(url: string, payload: T, params: HttpParams): Observable<T> {
     return this.http.post<T>(url, payload, { params }).pipe(timeout(this.HTTP_TIMEOUT), retry(2), catchError((err: HttpErrorResponse) => this.handleError<T>(err, url, payload, 'post', params)), share())
   }
-  public tryPut<T>(url: string, payload: T, params?: HttpParams): Observable<T> {
+  /**
+   * Acts as a proxy for all outgoing HTTP PUT requests. If a request could be made successfully,
+   * the response is returned from the request. If a backend error occurs, error is handled accordingly,
+   * if a network error occurs, queue the request for syncing.
+   * @param url 
+   * @param payload 
+   * @param params 
+   * @returns Observable
+   */
+  public tryPut<T>(url: string, payload: T, params: HttpParams): Observable<T> {
     return this.http.put<T>(url, payload, { params }).pipe(timeout(this.HTTP_TIMEOUT), retry(2), catchError((err: HttpErrorResponse) => this.handleError(err, url, payload, 'put', params)), share())
   }
-  private handleError<T>(err: HttpErrorResponse, url: string, payload: T, method: 'post' | 'put', params?: HttpParams): Observable<any> {
+  /**
+   * Helper function that resolves a HTTP error response to either a queue of the request
+   * or an RXJS error response in case of a backend fault.
+   * @param err 
+   * @param url 
+   * @param payload 
+   * @param method 
+   * @param params 
+   * @returns Observable
+   */
+  private handleError<T>(err: HttpErrorResponse, url: string, payload: T, method: 'post' | 'put', params: HttpParams): Observable<any> {
     if (this.offlineOrBadConnection(err)) {
       // A client-side or network error occurred. Handle it accordingly.
       console.log('handling error due to bad connection');
-      this.addOrUpdateSyncTask<T>(url, payload, method, params);
-      console.log('method called with', { url, payload, method, params });
       // add call to queue to retry at a later time
+      this.addOrUpdateSyncTask<T>(url, payload, method, params);
       return EMPTY
     } else {
       console.log('A backend error occurred.', err);
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong.
-      return throwError(err)
+      throw err
     }
   }
+  /**
+   * Helper function that determines whether an error was frontend or backend related.
+   * @param err 
+   * @returns true if error was locally caused and false if it was backend related
+   */
   private offlineOrBadConnection(err: HttpErrorResponse): boolean {
     return (
       err instanceof TimeoutError ||
       err.error instanceof ErrorEvent ||
-      !this.connection.getOnlineStatus()  // A helper service that delegates to window.navigator.onLine
+      !this.connection.getOnlineStatus()  // A helper service that delegates connectivity with AJAX requests
     );
   }
-  private addOrUpdateSyncTask<T>(url: string, payload: T, method: 'post' | 'put', params?: HttpParams): void {
+  /**
+   * Adds a HTTP request to the queue with the help of localForage indexedDB
+   * @param url 
+   * @param payload 
+   * @param method 
+   * @param params 
+   */
+  private addOrUpdateSyncTask<T>(url: string, payload: T, method: 'post' | 'put', params: HttpParams): void {
     this.getExistingSyncTasks().subscribe(tasks => {
-      console.log('tasks', tasks);
-      const syncTodo = new SyncToDo<T>(url, payload, params ? params.toString() : null, method);
+      const syncTodo = new SyncToDo<T>(url, payload, params.toString(), method);
       tasks.push(syncTodo);
-      console.log('syncToDo', syncTodo);
-      // localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
-      localforage.setItem('pendingTodos', tasks).then(x => {
+      localforage.setItem('pendingTodos', tasks).then(_ => {
         console.log('added new items to pendingTodos');
         alert('queued a new request locally :D')
       })
     })
   }
-
+  /**
+   * Queries the indexedDB with localForage to retrieve the queue of pending requests.
+   * @returns an Observable array of synced todos
+   */
   private getExistingSyncTasks(): Observable<SyncToDo<any>[]> {
     return new Observable(observer => {
       localforage.getItem('pendingTodos').then((tasks: any) => {
-        console.log('serialized tasks', tasks);
-        observer.next(tasks)
+        observer.next(tasks ?? [])
         observer.complete()
       })
     })
   }
+  /**
+   * Kicks off the syncing procedure and sends all pending requests in order of the queue.
+   * @returns an observable of synced todos
+   */
   public sync(): Observable<any> {
     return new Observable(observer => {
       this.getExistingSyncTasks().subscribe(syncTasks => {
-        let requests: Observable<any>[] = []
+        let requests: Observable<SyncToDo<any>>[] = []
         syncTasks.forEach((task: SyncToDo<any>) => {
-          const params = task.params ? { params: new HttpParams({ fromString: task.params }) } : {}
+          const params = { params: new HttpParams({ fromString: task.params }) }
           const response$ = this.http[task.method](task.url, task.body, params).pipe(map(_ => task), retry(2))
           requests.push(response$);
         })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ConnectionService } from './ConnectionService/connection.service';
 import { SyncService } from './SyncService/sync.service';
@@ -11,14 +11,14 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public todos: any[]
+  public todos: any[] = []
   public connectionState: boolean = true
   public offlineClicked: boolean = false
   public onlineTodoSent: boolean = false
   public pendingTodos: number = 0
   constructor(private http: HttpClient, private connection: ConnectionService, private sync: SyncService) { }
   //call the fake API endpoint to fetch some data. Easy enough.
-  fetchTodos() {
+  public fetchTodos() {
     if (!this.connectionState) this.offlineClicked = true
     this.http.get<any[]>("https://jsonplaceholder.typicode.com/todos").subscribe(todos => {
       console.log(todos);
@@ -26,14 +26,15 @@ export class AppComponent implements OnInit {
     })
   }
 
-  clearTodos() {
+  public clearTodos() {
     this.todos = []
   }
 
-  sendTodo(): void {
+  public sendTodo(): void {
+    const params = new HttpParams()
     this.sync.tryPost<{ id: number, title: string, body: string, userId: number }>("https://jsonplaceholder.typicode.com/todos", {
       id: 23, title: "Make an awesome app", body: "Revolutionize the App industry", userId: 2323
-    }).subscribe({
+    }, params).subscribe({
       next: response => {
         console.log('successfully posted!', response)
         alert('Todo posted successfully!')
@@ -49,9 +50,9 @@ export class AppComponent implements OnInit {
         setTimeout(() => {
           console.log('start sync');
           this.sync.sync().subscribe(x => {
-            console.log('sync results', x);
+            console.log('synced successfully');
           })
-        }, 4000)
+        }, 2000)
       }
       this.connectionState = status === 1
       console.log('connection', this.connectionState);
